5398413d4540011e0a327d69f9937157a28145cb
David Schwehr <dpschwehr@yahoo.com>
2016-09-17 13:05:56 -0600

    a start to the server.py structure

--- server.py
+++ server.py
@@ -1,24 +1,54 @@
+import sys
 import socket
 
 def Main():
-    host = '127.0.0.1'
-    port = 2526
+    host = '127.0.0.1'  # this may change
+    port = sys.argv[-2]
+    file_name = sys.argv[-1]  # name of text file for baord, may have to cast to string
+    h, w = 10, 10    # maybe use 2d array for board
 
-    s = socket.socket()
-    s.bind((host, port))
+    board_file.open((str)file_name, 'r')  # r for read only, r+ for read and write, w for write
+    # create 2d array or other text file here
+    # by reading from board_file object ie board_file.read() etc
 
-    s.listen(1)
-    c, addr = s.accept()
-    print 'Connection from: ' + str(addr)
+    # create connection and listen for client
+    sock = socket.socket()
+    sock.bind((host, port))
+    sock.listen(1)
+    conn, addr = sock.accept()  # conn is connection object
+
+    print 'Connection from: ' + str(addr)  # used for testing/debug only
+
+    # this will run until killed by user, client will be invoked each play
+    # all other functionallity will be done by calling other methods
     while True:
-        data = c.recv(1024)
+        data = conn.recv(1024)
+        # maybe do a try except here??
         if not data:
             break
-        print 'from connected user: ' + str(data)
-        data = 'put something here'
-        print 'sending: ' + str(data)
-        c.send(data)
-    c.close()
+        print 'from connected user: ' + str(data)  # used for testing/debug only
+
+        data = 'put something here'                # data will be x,y coords from client
+        #read_board(data)
+        reply = process_request()  # reply will be message returned to client
+        conn.send(reply)
+    conn.close()
+
+
+# read_board() used for reading board at coords sent from client
+# will return hit, miss, sunk to be sent to reply method to be replied
+# to clinet
+def read_board():
+
+
+# update_board() will record state of board
+def update_board():
+
+
+# process_reply() will take result of read_board and
+# return reply variable to send proper reply back to client via conn.send(reply)
+def process_reply():
+
 
 if __name__ == '__main__':
     Main()

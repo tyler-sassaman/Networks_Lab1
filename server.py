7ddf204cad9d853e248b02bad39c631610a9fc01
David Schwehr <dpschwehr@yahoo.com>
2016-09-19 16:45:37 -0600

    nearly finished methods for sever.py

--- server.py
+++ server.py
@@ -60,18 +60,98 @@ def Main():
 # read_board() used for reading board at coords sent from client
 # will return hit, miss, sunk to be sent to reply method to be replied
 # to clinet
-def read_board():
-    print 'read_board() not done, need to finish'
+def read_board(x, y):
+    global h
+    global w
+    global board
+    global submarine
+    global carrier
+    global battleship
+    global cRuiser
+    global destroyer
+    result = n     # initialize result to something
+    boat = t       # variable to send what ship to check if sunk to processs reply
+
+    if x >= w || x < 0:
+        result = o   # letter o for out of bounds 
+        return result
+    if y >= h || y < 0:
+        result = o    # letter o
+        return result
+    if board[x][y] = '-':
+        result = m
+        return result    # m for miss
+    if board[x][y] = 'S':
+        submarine -= 1
+        boat = 'sub'
+        result = h
+        return result
+    if board[x][y] = 'B':
+        battleship -= 1
+        boat = 'bs'
+        result = h
+        return result
+    if board[x][y] = 'C':
+        carrier -= 1
+        boat = 'car'
+        result = h
+        return result
+    if board[x][y] = 'D':
+        destroyer -= 1
+        boat = 'des'
+        result = h
+        return result
+    if board[x][y] = 'R':
+        cRuiser -= 1
+        boat = 'cru'
+        result = h
+        return result
+    if board[x][y] = 'X':
+        result = x
+        return result
+    update_board(x, y, result)
+    process_reply(result, boat)
 
 # update_board() will record state of board
-def update_board():
-    print 'update_board() not done, need to finish'
+def update_board(x, y, result):
+    global board
+    if result = m:
+        board[x][y] = 'X'
+    else:
+        board[x][y] = result
 
 
 # process_reply() will take result of read_board and
 # return reply variable to send proper reply back to client via conn.send(reply)
-def process_reply():
-    print 'process_reply() not done, need to finish'
+def process_reply(result, boat):
+    global submarine
+    global cRuiser
+    global battleship
+    global destroyer
+    global carrier
+
+    if boat = 'sub':
+        if submarine = 0:
+            # send http reply for sunk submarine
+    if boat = 'car':
+        if carrier = 0:
+            # send sunk carrier http
+    if boat = 'cru':
+        if cRuiser = 0:
+            # send sunk cruiser http
+    if boat = 'des':
+        if destroyer = 0:
+            # send sund destroyer http
+    if boat = 'bs':
+        if battleship = 0:
+            # send sunk battleship http
+    # if we get this far send result
+    if result = h:
+    if result = m:
+    if result = o:
+    if result = x:
+        # send proper http response
+    
 
 if __name__ == '__main__':
     Main()
